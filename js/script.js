4823d86631a1f221266ff962b7e023bf5204999e
Md Arif Hosain <arifhosain20284@gmail.com>
2024-03-19 12:36:51 +0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+//  Mega Menu Script
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+
+$('.carousel .ww').each(function () {
+  var minPerSlide = 4;
+  var next = $(this).next();
+  if (!next.length) {
+  next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+  for (var i = 0; i < minPerSlide; i++) { next=next.next(); if (!next.length) { next=$(this).siblings(':first'); }
+   next.children(':first-child').clone().appendTo($(this)); } });
+
+
+// Popup Form Java script
+   function openForm() {
+     document.getElementById("myForm").style.display = "block";
+   }
+   
+   function closeForm() {
+     document.getElementById("myForm").style.display = "none";
+   }
